a774ef4d291c7f2133e7b796f3e938545b889aa0
Tanisha Kumari <tanishakumari34703@gmail.com>
2026-07-30 03:46:56 +0530

    fix: add PORT for render deployment

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ app.get("/", (req, res) => {
   res.send("LMS Backend is running!");
 });
 
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
   console.log(`The server is running : http://localhost:${PORT}`);
 });
